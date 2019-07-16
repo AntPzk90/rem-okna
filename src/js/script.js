ee35def7d4d1c6d5a08cdb5631a42e7791016f90
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-07-16 22:08:30 +0300

    add jqery

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,51 @@
 $(document).ready(function(){
+    // размер контенера ( чтоб понять какой перед нами экран )
+    var bodyWidth = $("body").width();
+    console.log(bodyWidth);
+    // работа с шапкой сайта(если сайт скролился то добавляем модификатор)
+    var header =  $(".header");
+    if(bodyWidth < 750){
+        $(window).on("scroll", function(){
+            header.addClass("header--mobile");
+            $(".header__mobile").addClass("header__mobile--open");
+            $(".represent").css("paddingTop","150px")
+        });
+    }
+    // клин на бургер
+    $(".header__mobile").on("click", function(evt){
+        if($(".header__mobile").hasClass("header__mobile--open")){
+            $(".header__mobile").removeClass("header__mobile--open");
+            $(".header__mobile").addClass("header__mobile--close");
+            header.removeClass("header--mobile");
+        }
+        else{
+            header.addClass("header--mobile");
+            $(".header__mobile").addClass("header__mobile--open");
+        }
+    })
+    if($(window).scrollTop()!= 0){
+        header.addClass("header--fixed");
+    }
+    // фиксация шапки при скроле
 	$(window).on("scroll", function(){
         var header =  $(".header");
 	    var headHeight = header.height();
         var scroll =  $(window).scrollTop();
 	   if(scroll > headHeight){
-	      header.addClass("header--fixed").fadeIn(1000);
-          //$(".represent").css("paddingTop", headHeight);
+          header.addClass("header--fixed").fadeIn(1000);
 	    }else if(scroll == 0){
 	      header.removeClass("header--fixed");
-	      $(".represent").removeAttr('style');
 	    };
     });
-    var bodyWidth = $(".container").width();
+    //работа с показом кнопки номера телефона
     var telephoneBtnFunction = function(sizeVp){
         if(sizeVp >= 1050){
             $(".telephone").removeClass("telephone--close")
         }
     }
-    console.log(bodyWidth)
+    // если версия десктоп то номер по умолчанию показан
     telephoneBtnFunction(bodyWidth);
+    // работа с появлением попапа
     var telBtnClickHendler = function(evt){
         evt.preventDefault();
         $()
@@ -31,7 +58,24 @@ $(document).ready(function(){
             $(".telephone").addClass("telephone--close");
             $(".master-call__link").fadeOut(300);
         }
-        console.log(1)
     }
     $(".telephone__btn").on("click",telBtnClickHendler);
+    var popupBtnClickHendler = function(evt){
+        evt.preventDefault();
+        $("body").append('<div class="overlay"></div>');
+        $(".modal-form__form").addClass("modal-form__form--active");
+        $(".overlay").on("click",function(evt){
+            evt.preventDefault();
+            $(".overlay").fadeOut(300).remove();
+            $(".modal-form__form").removeClass("modal-form__form--active");
+        });
+    };
+    $(".master-call").on("click",popupBtnClickHendler);
+    // работа с закрытием попапа.
+    var closeBtnClickHendler = function(evt){
+        evt.preventDefault();
+        $(".modal-form__form").removeClass("modal-form__form--active");
+        $(".overlay").fadeOut(300).remove();
+    }
+    $(".modal-form__close-btn").on("click",closeBtnClickHendler);
 });
